9a9627d91389cb2dca685bc0da2b5b6151452a5b
yyysolhhh <solbaram37@gmail.com>
2025-03-10 13:03:59 +0900

    feat: 공통 필드 클래스 생성
    - createdAt, updatedAt 공통 필드로 생성
    - jpa auditing 활용하여 자동으로 시간 매핑
    - Application 클래스에 @EnableJpaAuditing 추가

--- src/main/java/com/kernel360/ronaldo/TemuOverflow/TemuOverflowApplication.java
+++ src/main/java/com/kernel360/ronaldo/TemuOverflow/TemuOverflowApplication.java
@@ -2,7 +2,9 @@ package com.kernel360.ronaldo.TemuOverflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TemuOverflowApplication {
 
