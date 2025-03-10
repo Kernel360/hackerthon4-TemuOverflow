5203df178cde5ccaef6aa326841fcdd239eaa694
HoyiTT <pjs9177@naver.com>
2025-03-10 11:03:41 +0900

    first commit

---
+++ src/main/java/com/kernel360/ronaldo/TemuOverflow/TemuOverflowApplication.java
@@ -0,0 +1,13 @@
+package com.kernel360.ronaldo.TemuOverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemuOverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemuOverflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kernel360/ronaldo/TemuOverflow/TemuOverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kernel360.ronaldo.TemuOverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemuOverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
