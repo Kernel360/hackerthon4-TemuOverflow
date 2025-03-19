0487ec920e53c390d0426ee6eea67e30ec583290
HoyiTT <pjs9177@naver.com>
2025-03-19 21:15:38 +0900

    feat :  db 이전 작업 테이블 reply, post 임시 변경

--- src/main/java/com/kernel360/ronaldo/TemuOverflow/post/entity/Post.java
+++ src/main/java/com/kernel360/ronaldo/TemuOverflow/post/entity/Post.java
@@ -23,11 +23,15 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id", nullable = false)
+//    @JoinTable(name = "users",
+//            joinColumns = @JoinColumn(name = "post_id"),
+//            inverseJoinColumns = @JoinColumn(name = "id"))
+//    private User user;
+
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    @JoinTable(name = "users",
-            joinColumns = @JoinColumn(name = "post_id"),
-            inverseJoinColumns = @JoinColumn(name = "id"))
+    @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
     private User user;
 
     @Column(nullable = false, length = 45)
